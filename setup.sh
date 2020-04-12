f51c4d416c220e0b97bd49ca8ec9b5c4671c312f
satetheus <chriswarrenmccullough@gmail.com>
2020-04-11 22:41:05 -0600

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#! usr/bin/env bash
+ln -s .gitignore_global ~/.gitignore_global
+# will add .gitconfig later
+ln -s .bashrc ~/.bashrc
+ln -s .bash_aliases ~/.bash_aliases 
+ln -s .vimrc ~/.vimrc
+ln -s deus.vim ~/.vim/deus.vim
