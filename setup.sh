e5a01ef6704fdd4801b98eacf20afdb032f5800c
cwmccullough <chris.mccullough1@nutrien.com>
2020-04-15 14:51:23 -0600

    added config for git ignore to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #! usr/bin/env bash
+
+# setup links for config files
 ln ~/dotfiles/.gitignore_global ~/.gitignore_global
 # will add .gitconfig later
 ln ~/dotfiles/.bashrc ~/.bashrc
 ln ~/dotfiles/.bash_aliases ~/.bash_aliases 
 ln ~/dotfiles/.vimrc ~/.vimrc
 ln ~/dotfiles/deus.vim ~/.vim/colors/deus.vim
+
+# setup global git ignore
+git config --global core.excludesfile ~/.gitignore_global
