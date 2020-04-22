3ab5fff2aba92ade296c6bb6ae9449ee6a416138
satetheus <chriswarrenmcculloughwork@gmail.com>
2020-04-21 22:32:25 -0600

    added fzf installs to setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,11 @@ ln -s ~/dotfiles/deus.vim ~/.vim/colors/deus.vim
 
 # setup global git ignore
 git config --global core.excludesfile ~/.gitignore_global
+
+# install fzf, could be replaced by 'apt-get install fzf' theorectically
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# install fzf plugin for vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
