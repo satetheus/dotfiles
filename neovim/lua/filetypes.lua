0411d9cd735397d3758df04b05b3cf7e6e7e83a4
satetheus <chriswarrenmccullough@gmail.com>
2023-11-11 16:42:46 -0700

    set nvim open help files vert-right & large file handling draft

--- neovim/lua/filetypes.lua
+++ neovim/lua/filetypes.lua
@@ -8,3 +8,20 @@ augroup templates
   autocmd BufNewFile *.js 0r ~/.config/nvim/templates/skeleton.js
 augroup END
 ]])
+
+vim.cmd([[
+augroup helpfiles
+  au!
+  au BufEnter */doc/* if &filetype=='help' | wincmd L | endif
+]])
+
+-- large file handling
+vim.cmd([[
+if !exists("my_auto_commands_loaded")
+    let my_auto_commands_loaded = 1
+    let g:LargeFile = 1024 * 1024 * 10
+    augroup LargeFile
+        autocmd BufReadPre * let f=expand("<afile>") | if getfsize(f) > g:LargeFile | set eventignore+=FileType | setlocal noswapfile bufhidden=unload buftype=nowrite undolevels=-1 | else | set eventignore-=FileType | endif
+    augroup END
+endif
+]])
